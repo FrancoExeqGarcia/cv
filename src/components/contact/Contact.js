8c9e654cd740fc1f60a1788eb5c5344992523ee8
FrancoExeqGarcia <francoexequiel.garcia150@gmail.com>
2024-07-29 22:35:49 -0300

    cambios

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "../contact/contact.css";
 
 const Contact = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    setIsVisible(!isVisible);
+  };
+
   return (
     <div className="contact">
-      <h3>CONTACTO</h3>
-      <p>Teléfono: (+549) 341-6991069</p>
-      <p>Mail: francoexequiel_garcia@hotmail.com</p>
-      <p>Dirección: Urquiza 2706 - Rosario - Santa Fe</p>
+      <button onClick={toggleVisibility}>
+        {isVisible ? 'Ocultar Información' : 'Mostrar Información'}
+      </button>
+      {isVisible && (
+        <div className="contact-info">
+          <p>Teléfono: (+549) 341-6991069</p>
+          <p>Mail: francoexequiel_garcia@hotmail.com</p>
+          <p>Dirección: Urquiza 2706 - Rosario - Santa Fe</p>
+        </div>
+      )}
     </div>
   );
 };
